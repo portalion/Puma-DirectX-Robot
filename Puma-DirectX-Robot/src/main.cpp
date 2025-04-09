6474d68d9bd7f5fc0c9675e508631852b9a9c321
MiloszS <m.sobczyk.official@gmail.com>
2025-04-09 12:59:08 +0200

    Add project files.

---
+++ Puma-DirectX-Robot/src/main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+#include <exception>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
+{
+	UNREFERENCED_PARAMETER(cmdLine);
+	auto exitCode = EXIT_SUCCESS;
+	return exitCode;
+}
